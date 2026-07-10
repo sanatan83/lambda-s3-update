7fb49ba40e382ba5c89d3813e25cc181ff52e7a5
sanatan <sanatan.kumar85111@gmail.com>
2026-07-10 14:04:32 +0530

    update dfiles

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,3 +33,25 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+
+
+
+resource "aws_lambda_function" "s3_notification" {
+
+  function_name = "s3-upload-notification"
+
+  filename = "../lambda/lambda.zip"
+
+  source_code_hash = filebase64sha256("../lambda/lambda.zip")
+
+  role = aws_iam_role.lambda_role.arn
+
+  handler = "lambda_function.lambda_handler"
+
+  runtime = "python3.12"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs
+  ]
+}
