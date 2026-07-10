8f999e3f3faf7446394e196df2f63edd2d3fa1c9
sanatan <sanatan.kumar85111@gmail.com>
2026-07-10 14:27:00 +0530

    notification added

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,19 @@ resource "aws_lambda_function" "s3_notification" {
   ]
 }
 
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.s3_notification.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [
+    aws_lambda_permission.allow_s3
+  ]
+}
+
 
 
 resource "aws_lambda_permission" "allow_s3" {
